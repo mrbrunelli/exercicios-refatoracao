f5a05075411096d30d42c98759f8e85a14ece5f9
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-02-07 14:15:37 -0300

    refactor: change format name to brl

--- statement.js
+++ statement.js
@@ -8,11 +8,11 @@ export default function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     result += `${playFor(perf).name}: ${
-      format(amountFor(perf) / 100)
+      brl(amountFor(perf) / 100)
     } (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${brl(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -50,7 +50,7 @@ export default function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function brl(aNumber) {
     return `R$ ${aNumber}`;
   }
 }
