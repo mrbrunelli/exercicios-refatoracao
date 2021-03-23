2dac2a6767a715ce54ea2fb3072f8615a67692c7
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-23 07:18:10 -0300

    refactor: add reduce pipeline to totalVolumeCredits statement

--- statement.js
+++ statement.js
@@ -22,11 +22,7 @@ export default function statement(invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (const perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function volumeCreditsFor(aPerformance) {
