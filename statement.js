029ee98157f8135898196c8714cb72523e63581c
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-23 06:56:50 -0300

    feat: add shallow copy of performances array

--- statement.js
+++ statement.js
@@ -4,8 +4,13 @@ const plays = JSON.parse(Deno.readTextFileSync("./plays.json"));
 export default function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
