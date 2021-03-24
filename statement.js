ee158fbdcf9b8ac0b69bd294c92ce42639327403
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-24 06:54:15 -0300

    feat: add html render function

--- statement.js
+++ statement.js
@@ -1,13 +1,9 @@
 const invoices = JSON.parse(Deno.readTextFileSync("./invoices.json"));
 const plays = JSON.parse(Deno.readTextFileSync("./plays.json"));
-import createStatementData from './createStatementData.js';
+import createStatementData from "./createStatementData.js";
 
 export default function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
-}
-
-function brl(aNumber) {
-  return `R$ ${aNumber / 100}`;
 }
 
 function renderPlainText(data, plays) {
@@ -20,5 +16,26 @@ function renderPlainText(data, plays) {
   result += `Amount owed is ${brl(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (const perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${brl(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${brl(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+function brl(aNumber) {
+  return `R$ ${aNumber / 100}`;
 }
