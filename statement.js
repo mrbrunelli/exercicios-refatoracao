83a94bf616e7c6e22d3f955230c66d6ca1cfa6a3
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-24 06:26:00 -0300

    refactor: transfer division for 100 to brl function

--- statement.js
+++ statement.js
@@ -6,18 +6,19 @@ export default function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function brl(aNumber) {
+  return `R$ ${aNumber / 100}`;
+}
+
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (const perf of data.performances) {
     result += `${perf.play.name}: ${
-      brl(perf.amount / 100)
+      brl(perf.amount)
     } (${perf.audience} seats)\n`;
   }
-  result += `Amount owed is ${brl(data.totalAmount / 100)}\n`;
+  result += `Amount owed is ${brl(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 
-  function brl(aNumber) {
-    return `R$ ${aNumber}`;
-  }
 }
