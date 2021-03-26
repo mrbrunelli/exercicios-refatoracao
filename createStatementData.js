702ab90f14b16601dff8aa99592970c589490be3
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-26 06:39:04 -0300

    refactor: remove unused methods

--- createStatementData.js
+++ createStatementData.js
@@ -1,10 +1,10 @@
 export default function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const calculator = createPerformanceCalculator(
@@ -18,26 +18,16 @@ export default function createStatementData(invoice, plays) {
     return result;
   }
 
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(
-      aPerformance,
-      playFor(aPerformance),
-    ).amount;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 }
 
