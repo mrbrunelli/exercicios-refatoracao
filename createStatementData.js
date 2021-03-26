db889f48869dc2e33b3981ca20e7fb7f78f36bcf
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-26 06:36:08 -0300

    refactor: transfer volumeCredits calc to children class

--- createStatementData.js
+++ createStatementData.js
@@ -59,12 +59,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) {
-      result += Math.floor(this.performance.audience / 5);
-    }
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 
   get amount() {
@@ -90,5 +85,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
